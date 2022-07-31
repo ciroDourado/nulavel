2d8a79ef94dad5998267ec72c69e8d47be31de3e
ciroDourado <ciro.brz@gmail.com>
2022-07-31 03:19:06 -0300

    feature: testes para metodo entao

--- test/nulavel_test.exs
+++ test/nulavel_test.exs
@@ -10,4 +10,24 @@ defmodule NulavelTest do
     assert 1 |> Nulavel.ok? |> Nulavel.e_ok
   end
 
+  test "Operação apenas em não-nulos" do
+    dobrar    = fn numero -> numero * 2 end
+    triplicar = fn numero -> numero * 3 end
+    resultado = Nulavel.ok(1)
+    |> Nulavel.entao(dobrar)
+    |> Nulavel.entao(triplicar)
+
+    assert resultado.valor == 6
+  end
+
+  test "Sem operação em valores nulos" do
+    dobrar    = fn numero -> numero * 2 end
+    triplicar = fn numero -> numero * 3 end
+    resultado = Nulavel.ok?(nil)
+    |> Nulavel.entao(dobrar)
+    |> Nulavel.entao(triplicar)
+
+    assert resultado.valor == nil
+  end
+
 end
